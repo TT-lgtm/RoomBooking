a78cb11b8172738f5bd77fee088938fee4629d68
Valeria <comkosmos.sch@gmail.com>
2026-09-04 11:33:37 +0300

    Добавить слой логики и вывод результата

---
+++ RoomBooking.Logic/BookingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using RoomBooking.Data;
+
+namespace RoomBooking.Logic
+{
+    public class BookingService
+    {
+        private readonly BookingRepository _repository = new();
+        public List<Booking> GetImportant()
+        {
+            return _repository.GetAll().Where(item => item.Hour > 14).ToList();
+        }
+    }
+}
