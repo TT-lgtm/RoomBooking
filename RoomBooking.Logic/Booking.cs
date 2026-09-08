71cea456b25bbb912e69cea94b81704e674adcd7
Valeria <comkosmos.sch@gmail.com>
2026-09-08 15:59:04 +0300

    Ввести контракт хранилища в слое логики

---
+++ RoomBooking.Logic/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBooking.Logic
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string Room { get; set; } = "";
+        public int Hour { get; set; }
+    }
+}
--- RoomBooking.Logic/BookingService.cs
+++ RoomBooking.Logic/BookingService.cs
@@ -4,16 +4,21 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-using RoomBooking.Data;
 
 namespace RoomBooking.Logic
 {
     public class BookingService
     {
-        private readonly BookingRepository _repository = new();
+        private readonly IBookingRepository _repository;
+        public BookingService(IBookingRepository repository)
+        {
+            _repository = repository;
+        }
         public List<Booking> GetImportant()
         {
-            return _repository.GetAll().Where(item => item.Hour > 14).ToList();
+            return _repository.GetAll()
+            .Where(item => item.Hour > 14)
+            .ToList();
         }
     }
 }
